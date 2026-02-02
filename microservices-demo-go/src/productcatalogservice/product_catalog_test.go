3691af7cfb4e41c094cecbd249406fd079a3d669
justinrosales <rosalesjpn@gmail.com>
2026-02-02 01:42:35 -0800

    test(productcatalog): add logic and handler tests for REST port

---
+++ microservices-demo-go/src/productcatalogservice/product_catalog_test.go
@@ -0,0 +1,79 @@
+package main
+
+import (
+	"context"
+	"testing"
+)
+
+var (
+	mockProductCatalog *productCatalog
+)
+
+func setupMock() {
+	mockProductCatalog = &productCatalog{
+		catalog: Catalog{
+			Products: []*Product{},
+		},
+	}
+
+	mockProductCatalog.catalog.Products = append(mockProductCatalog.catalog.Products, &Product{
+		ID:   "abc001",
+		Name: "Product Alpha One",
+	})
+	mockProductCatalog.catalog.Products = append(mockProductCatalog.catalog.Products, &Product{
+		ID:   "abc002",
+		Name: "Product Delta",
+	})
+	mockProductCatalog.catalog.Products = append(mockProductCatalog.catalog.Products, &Product{
+		ID:   "abc003",
+		Name: "Product Alpha Two",
+	})
+	mockProductCatalog.catalog.Products = append(mockProductCatalog.catalog.Products, &Product{
+		ID:   "abc004",
+		Name: "Product Gamma",
+	})
+}
+
+func TestGetProductExists(t *testing.T) {
+	setupMock()
+	product, err := mockProductCatalog.GetProduct(context.Background(), "abc003")
+	if err != nil {
+		t.Fatal(err)
+	}
+	if got, want := product.Name, "Product Alpha Two"; got != want {
+		t.Errorf("got %s, want %s", got, want)
+	}
+}
+
+func TestGetProductNotFound(t *testing.T) {
+	setupMock()
+	_, err := mockProductCatalog.GetProduct(context.Background(), "abc005")
+	if err == nil {
+		t.Fatal("expected error, got nil")
+	}
+	if got, want := err.Error(), "no product with ID abc005"; got != want {
+		t.Errorf("got %s, want %s", got, want)
+	}
+}
+
+func TestListProducts(t *testing.T) {
+	setupMock()
+	products, err := mockProductCatalog.ListProducts(context.Background())
+	if err != nil {
+		t.Fatal(err)
+	}
+	if got, want := len(products), 4; got != want {
+		t.Errorf("got %d, want %d", got, want)
+	}
+}
+
+func TestSearchProducts(t *testing.T) {
+	setupMock()
+	products, err := mockProductCatalog.SearchProducts(context.Background(), "alpha")
+	if err != nil {
+		t.Fatal(err)
+	}
+	if got, want := len(products), 2; got != want {
+		t.Errorf("got %d, want %d", got, want)
+	}
+}
